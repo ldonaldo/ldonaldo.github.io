9c110803aa2366bc04a80c0b1934fcbec81fc441
DONALDO\Donaldo <ldonaldo@uninorte.edu.co>
2020-10-01 21:17:55 -0500

    Updates

---
+++ precache-manifest.5b9c6594530360e7bcbd6cf42a200168.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d6550ab93fb8e46b3baefe35a93a9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54120c673133a70410e1",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3bcea4fb0ea6f4f7759c",
+    "url": "/static/css/main.3cb86ba8.chunk.css"
+  },
+  {
+    "revision": "54120c673133a70410e1",
+    "url": "/static/js/2.8f8e07e1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8f8e07e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bcea4fb0ea6f4f7759c",
+    "url": "/static/js/main.34883357.chunk.js"
+  },
+  {
+    "revision": "40b75bf57960df7bc0e8",
+    "url": "/static/js/runtime-main.d6f6ceba.js"
+  },
+  {
+    "revision": "358a2035508079adc2e594a397a52422",
+    "url": "/static/media/Donaldo.358a2035.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
